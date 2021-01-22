3cdd7dad6849c42c511cb38d9646406d8a95fcf4
WillianFarias <williansousafarias@gmail.com>
2021-01-22 11:42:17 -0300

    corrigingo bug quando já está pulando

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const dino = document.querySelector('.dino');
+let isJumping = false;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    //console.log('Precionou espaço');
-    jump();
+    if (!isJumping) {
+      jump();
+    }
   }
 }
 
 function jump() {
   let position = 0;
+
+  isJumping = true;
 
   //setInterval define intervalos
   let upInterval = setInterval(() => {
@@ -18,6 +22,7 @@ function jump() {
       let downInterval = setInterval(() => {
         if (position <= 0) {
           clearInterval(downInterval);
+          isJumping = false;
         } else {
           position -= 20;
           dino.style.bottom = position + 'px';
