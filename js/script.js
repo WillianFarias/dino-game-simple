4db74c0ac7f286b1843d535c90c99bb0075423a5
WillianFarias <williansousafarias@gmail.com>
2021-01-22 11:39:04 -0300

    criando animacao de pulo do dino

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 const dino = document.querySelector('.dino');
 
 function handleKeyUp(event) {
-  if(event.keyCode === 32) {
-    console.log('Precionou espaço');
+  if (event.keyCode === 32) {
+    //console.log('Precionou espaço');
+    jump();
   }
+}
+
+function jump() {
+  let position = 0;
+
+  //setInterval define intervalos
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      });
+    } else {
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
